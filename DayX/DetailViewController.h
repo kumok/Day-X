65d76e89af3d0106adcfad42867624ddfe464d04
kumok <bhh@byu.edu>
2015-01-13 20:29:39 -0700

    step 4

---
+++ DayX/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  DayX
+//
+//  Created by Duc Ho on 1/13/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
